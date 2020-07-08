55238232848f76102e6500d6366ed49b0f5d72ac
bertil291utn <bertilt4@gmail.com>
2020-07-08 16:33:47 -0500

    Ganeboard and player object - Create first two objects in order to achieve tic tac to game

---
+++ assets/javascript/script.js
@@ -0,0 +1,17 @@
+const Player = (name, tag) => {
+  const getName = () => name;
+  const getTag = () => tag;
+
+  const move = () => {
+
+  };
+};
+
+const GameBoard = () => {
+  const winningGame = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+
+  const gameBoard = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+};
+
+// Sol
