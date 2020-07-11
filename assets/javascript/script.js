9b5de6a2768dc45dd80f0c641dfeda778e1e29b9
bertil291utn <bertilt4@gmail.com>
2020-07-11 11:53:16 -0500

    Split adEventlistener callback - Add new function called GameFlowDOM to split in another function eventlistener callback

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,8 @@
 // const readline = require('readline-sync');
 const btn = document.querySelectorAll('[define-custom-id]');
 const status = document.querySelector('#status');
+const gameBoard = document.getElementsByClassName('btn');
+
 let stepCounter = 1;
 
 const Player = (name, tag) => {
@@ -11,13 +13,21 @@ const Player = (name, tag) => {
   return { getName, getTag, choices };
 };
 
-
 const GameBoard = (() => {
-  const winningGame = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+  const winningGame = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
   const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  const fullGameBoard = () => GameBoard.gameBoard.every(r => (/(X|O)/).test(r));
+  const fullGameBoard = () => GameBoard.gameBoard.every((r) => /(X|O)/.test(r));
 
   return {
     winningGame,
@@ -36,9 +46,10 @@ const GameFlow = (() => {
     player.choices.push(index);
   };
 
-  const displayBoard = () => { };
-
-  const gameOutcome = (player) => GameBoard.winningGame.some(winMatch => winMatch.every(r => player.choices.includes(r)));
+  const gameOutcome = (player) =>
+    GameBoard.winningGame.some((winMatch) =>
+      winMatch.every((r) => player.choices.includes(r))
+    );
 
   const winner = (player1, player2) => {
     if (GameFlow.gameOutcome(player1)) {
@@ -69,7 +80,6 @@ const GameFlow = (() => {
     return false;
   };
 
-
   return {
     addPlayer,
     makeMove,
@@ -83,98 +93,51 @@ const GameFlow = (() => {
 const DOM = (() => {
   const gameBoardRender = (gameBoard) => {
     gameBoard.forEach((elem, index) => {
-      btn[index].childNodes[0].innerHTML = typeof (elem) === 'number' ? '' : elem;
+      btn[index].childNodes[0].innerHTML = typeof elem === 'number' ? '' : elem;
     });
+  };
+
+  const gameFlowDOM = (player1, player2, index, objecto) => {
+    if (
+      !/(X|O)/.test(GameBoard.gameBoard[index]) &&
+      !GameFlow.theresWinner(player1, player2)
+    ) {
+      const btnIndex = objecto.getAttribute('define-custom-id');
+      const player = stepCounter % 2 === 0 ? player1 : player2;
+      GameFlow.makeMove(+btnIndex, player);
+      if (/(X|O)/.test(GameBoard.gameBoard[index])) {
+        btn[index].classList.remove('active-game-block');
+      }
+      DOM.gameBoardRender(GameBoard.gameBoard);
+      if (stepCounter >= 5) {
+        const message = GameFlow.winner(player1, player2);
+        status.innerHTML = message;
+      }
+      stepCounter += 1;
+    }
+    if (GameFlow.theresWinner(player1, player2)) {
+      GameBoard.gameBoard.forEach((e, i) => {
+        if (/\d/.test(e)) {
+          btn[i].classList.remove('active-game-block');
+        }
+      });
+    }
   };
 
   return {
     gameBoardRender,
+    gameFlowDOM,
   };
 })();
 
 const playerJason = GameFlow.addPlayer('Jason', 'X');
 const playerMark = GameFlow.addPlayer('Mark', 'O');
 
-const gameBoard = document.getElementsByClassName('btn');
-
-// GameBoard.gameBoard = [0, 1, 'O',
-//   'O', 'O', 'X',
-//   'X', 'X', 'O'];
-
 DOM.gameBoardRender(GameBoard.gameBoard);
 
 for (let i = 0; i < gameBoard.length; i += 1) {
   gameBoard[i].addEventListener('click', function name(e) {
-    if (!(/(X|O)/).test(GameBoard.gameBoard[i]) && !GameFlow.theresWinner(playerJason, playerMark)) {
-      // e.preventDefault();
-      const btnIndex = this.getAttribute("define-custom-id");
-      const player = stepCounter % 2 === 0 ? playerJason : playerMark;
-      GameFlow.makeMove(+btnIndex, player);
-      // if ((/(X|O)/).test(GameBoard.gameBoard[i])) {
-      //   btn[i].classList.remove('active-game-block');
-      // }
-      DOM.gameBoardRender(GameBoard.gameBoard);
-      if (stepCounter >= 5) {
-        const message = GameFlow.winner(playerJason, playerMark);
-        status.innerHTML = message;
-        console.log(GameFlow.theresWinner);
-      }
-      stepCounter += 1;
-    }
-    if (GameFlow.theresWinner(playerJason, playerMark)) {
-      GameBoard.gameBoard.forEach((e, i) => {
-        if ((/\d/).test(e)) {
-          btn[i].classList.remove('active-game-block');
-        }
-      })
-    }
+    e.preventDefault();
+    DOM.gameFlowDOM(playerJason, playerMark, i, this);
   });
 }
-
-
-// init play entering a position 
-// let index = 1;
-// while (!GameFlow.theresWinner(playerJason, playerMark) && !GameFlow.itsDraw()) {
-//   const player = index % 2 === 1 ? playerJason : playerMark;
-//   const indice = readline.question(`Enter index position ${player.getName()}: `);
-//   GameFlow.makeMove(+indice, player);
-
-//   if (index >= 5) {
-//     console.log(GameFlow.winner(playerJason, playerMark));
-//   }
-//   index += 1;
-// }
-
-//0,2,1,3,5,4,6,7,8  tie
-//0,1,2,3,4,5,6,7
-// playerJason.choices = [0, 1, 5, 6, 7];
-// playerMark.choices = [2, 3, 4, 8];
-// GameBoard.gameBoard = ['X', 'X', 'O',
-//                        'O', 'O', 'X',
-//                        'X', 'X', 'O'];
-// console.log(playerJason.choices);
-// console.log(playerMark.choices);
-// console.log(GameFlow.theresWinner(playerJason, playerMark));
-// console.log("GameOutcome:");
-// console.log(GameFlow.gameOutcome(playerJason));
-// console.log("Tie?:");
-// console.log(GameFlow.winner(playerJason, playerMark));
-// //0,2,1,3,5,4,6,7,8
-
-// console.log(GameBoard.gameBoard);
-// console.log(GameBoard.fullGameBoard());
-// console.log(GameBoard.gameBoard.every(r => (/(X|O)/).test(r)));
-
-// // Plan B
-// GameFlow.makeMove(+indice, player);
-// GameFlow.makeMove(+indice, player);
-// GameFlow.makeMove(+indice, player);
-// GameFlow.makeMove(+indice, player);
-// GameFlow.makeMove(+indice, player);
-// GameFlow.winner(playerJason, playerMark);
-// GameFlow.makeMove(+indice, player);
-// GameFlow.winner(playerJason, playerMark);
-// GameFlow.makeMove(+indice, player);
-// GameFlow.winner(playerJason, playerMark);
-// GameFlow.makeMove(+indice, player);
-// GameFlow.winner(playerJason, playerMark);
